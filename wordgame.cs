508fec28f3405e3bebe4bf3db814f5365f08149a
Ed Paradis <legomaniac@gmail.com>
2012-08-26 01:23:27 -0400

    distance method

--- wordgame.cs
+++ wordgame.cs
@@ -2,8 +2,29 @@ using System;
 
 public class WordGame
 {
-	static public void Main ()
+	static public int Main ( string[] args)
 	{
 		Console.WriteLine( "wordgame!");
+
+		Console.WriteLine( "distance from cat to dog: " + distance("cat", "dog"));
+		Console.WriteLine( "distance from cat to car: " + distance("cat", "car"));
+
+		return 0;
+	}
+
+	// returns the 'distance' between two words; which for this game is how many letters need to be changed between the two
+	static int distance( string A, string B)
+	{
+		if(A.Length != B.Length)
+			return -1;	// we do not consider unequal length words
+
+		int count = 0;
+		for( int i = 0; i<A.Length; i++)
+		{
+			if(A[i] != B[i])
+				count++;
+		}
+
+		return count;
 	}
 }
