887b1427579fa07cc21cee96f07aa39c2086b12d
Ed Paradis <legomaniac@gmail.com>
2012-08-26 01:56:50 -0400

    checks for check_game; group tests together

--- wordgame.cs
+++ wordgame.cs
@@ -7,6 +7,14 @@ public class WordGame
 	{
 		Console.WriteLine( "wordgame!");
 
+		do_tests();	// little tests of the utility functions
+
+		return 0;
+	}
+
+	// keep all the tests together
+	static void do_tests()
+	{
 		Console.WriteLine( "distance from cat to dog: " + distance("cat", "dog"));
 		Console.WriteLine( "distance from cat to car: " + distance("cat", "car"));
 
@@ -17,10 +25,8 @@ public class WordGame
 		Console.WriteLine( "game 'cat car bar' valid? " + check_game(setA));
 		setA.Add("dog");
 		Console.WriteLine( "game 'cat car bar dog' valid? " + check_game(setA));
-
-
-		return 0;
 	}
+
 
 	// returns the 'distance' between two words; which for this game is how many letters need to be changed between the two
 	static int distance( string A, string B)
@@ -41,6 +47,12 @@ public class WordGame
 	// given a list of words, make sure the distance between each word is only one
 	static bool check_game( List<string> words )
 	{
+		if( words.Count == 0)
+			return false;	// a list with nothing in it is never valid
+
+		if( words.Count == 1)
+			return true;	// a list with a single word is always valid
+
 		bool valid = true;
 		string prev = words[0];
 		for(int i = 1; i<words.Count; i++)
